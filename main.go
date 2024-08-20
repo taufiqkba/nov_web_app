42ee235ad3b659108103f966b4a8da81a6f28bbc
taufiqkba <taufiqkurniawan9991@gmail.com>
2024-08-20 11:30:20 +0700

    learn: hello world web app

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	message := "Welcome"
+	w.Write([]byte(message))
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	message := "Hello"
+	w.Write([]byte(message))
+}
+
+func main() {
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", handlerIndex)
+	http.HandleFunc("/hello", handlerHello)
+
+	address := "localhost:9000"
+	fmt.Printf("serve started at %s\n", address)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
