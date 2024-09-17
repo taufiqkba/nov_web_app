d9f90c5f0b2053cb0edd4718d8dcc9f5a7c88a9b
taufiqkba <taufiqkurniawan9991@gmail.com>
2024-09-17 10:58:49 +0700

    learn: routing static assets

--- routing/main.go
+++ routing/main.go
@@ -10,13 +10,21 @@ func main() {
 		w.Write([]byte("Welcome"))
 	}
 
-	http.HandleFunc("/", handlerIndex)
+	// http.HandleFunc("/", handlerIndex)
 	http.HandleFunc("/index", handlerIndex)
 
 	// using anonymous function
 	http.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello Again"))
 	})
+
+	// routing using static assets
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("assets"))))
+
+	http.Handle("/", http.FileServer(http.Dir("assets")))
+	http.Handle("/static", http.FileServer(http.Dir("assets")))
 
 	// running server
 	fmt.Println("server started at localhost:9000")
